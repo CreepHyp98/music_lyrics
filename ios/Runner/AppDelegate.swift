54081846cb499df986709c9941bc6b9517b55ec0
tkhw <tkhw.lr0505nr@mineo.jp>
2023-10-30 00:01:19 +0900

    iosバグのマージ漏れ

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -13,35 +13,12 @@ import MediaPlayer
     // ーーーーー追加ーーーーー
     do {
         if #available(iOS 10.0, *){
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, mode: AVAudioSessionModeDefault)
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default)
             try AVAudioSession.sharedInstance().setActive(true)
         }
     } catch {
         print(error)
     }
-    // MPRemoteCommandCenterのインスタンスを取得
-    let commandCenter = MPRemoteCommandCenter.shared()
-
-    // リモートコントロールイベントのハンドラを設定
-    commandCenter.playCommand.addTarget { [unowned self] event in
-        if canPlayMedia() {
-            // メディア再生のためのカスタムロジック
-            return .success
-        }
-        return .commandFailed
-    }
-
-    commandCenter.pauseCommand.addTarget { [unowned self] event in
-        if canPauseMedia() {
-            // メディア一時停止のためのカスタムロジック
-            return .success
-        }
-        return .commandFailed
-    }
-
-    // 他のリモートコントロールイベントのハンドラを設定
-    return true
-    // ーーーーーーーーーー
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
